568e6d3118f9aff2d90c9dbd01a889c81b97c385
Jackie Chen <ecuojackie@yahoo.com>
2022-07-20 20:27:45 -0700

    input and validate characters in the box

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -1,30 +1,43 @@
 import React, { useState } from "react";
 
+const AVAILABLE_LETTERS = "PURPLE".split("");
 
-
-const PlayScreen= () => {
-  let initialArr:string[] = []
-  for(let i = 0; i < 144; i++){
-    initialArr.push("")
-  }
-  const [tiles, setTiles] = useState(initialArr)
-  console.log(tiles)
-  const selectTile = (key: number) => {
-    const clickText:string = 'P'
-    setTiles([
-    ...tiles.slice(0, key),
-    clickText,
-    ...tiles.slice(key + 1, tiles.length)
-  ])
-  }
-  return <div className="grid grid-cols-12 gap-2 s:w-full md:w-1/2 lg:w-1/3 h-1/2">
-    
-    {tiles.map((value,key) => (
-      <div className="s:w-6 s:h-6 w-8 h-8 border-2 border-red-200 hover:border-blue-200 flex justify-center items-center" onClick={() => selectTile(key)} key={key}>{value}</div>
-    )
-    )}
-
-  </div>;
+const PlayScreen = () => {
+	let initialArr: string[] = [];
+	for (let i = 0; i < 144; i++) {
+		initialArr.push("");
+	}
+	const [tiles, setTiles] = useState(initialArr);
+	const selectTile = (key: number) => {
+		const inputTile = (event: any) => {
+			let inputChar;
+			if (AVAILABLE_LETTERS.includes(event.key.toUpperCase())) {
+				inputChar = event.key.toUpperCase();
+			}
+			setTiles([
+				...tiles.slice(0, key),
+				inputChar,
+				...tiles.slice(key + 1, tiles.length),
+			]);
+		};
+		if (tiles[key] === "") {
+			window.addEventListener("keydown", inputTile);
+			return () => window.removeEventListener("keydown", inputTile);
+		}
+	};
+	return (
+		<div className="grid grid-cols-12 gap-2 s:w-full md:w-1/2 lg:w-1/3 h-1/2">
+			{tiles.map((value, key) => (
+				<div
+					className="s:w-6 s:h-6 w-8 h-8 border-2 border-red-200 hover:border-blue-200 flex justify-center items-center"
+					onClick={() => selectTile(key)}
+					key={key}
+				>
+					{value}
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default PlayScreen;
