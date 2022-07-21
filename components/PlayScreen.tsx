5c972d952eaca83ac20f20541689c5e8fbcdcb73
Jackie Chen <ecuojackie@yahoo.com>
2022-07-21 00:49:12 -0700

    horizontal left to right condition checking

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -1,18 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AVAILABLE_LETTERS = "PURPLE".split("");
 
 const PlayScreen = () => {
 	let initialArr: string[] = [];
+
+	// For loops will generate an array of empty strings
 	for (let i = 0; i < 144; i++) {
 		initialArr.push("");
 	}
 	const [tiles, setTiles] = useState(initialArr);
+	const [selectedIndex, setSelectedIndex] = useState(-1);
+	const [isWin, setIsWin] = useState(false);
+
+	useEffect(() => {
+		let rightCharacters =
+			AVAILABLE_LETTERS.length -
+			1 -
+			AVAILABLE_LETTERS.indexOf(tiles[selectedIndex]);
+		let leftCharacters = AVAILABLE_LETTERS.indexOf(tiles[selectedIndex]);
+		//check word going right
+		let rightCheck = false;
+		let leftCheck = false;
+		if (rightCharacters === 0) rightCheck = true;
+		for (let i = 1; i < rightCharacters + 1; i++) {
+			//check right side of AVAILABLE_LETTERS array
+			if (rightCharacters === 0) rightCheck = true;
+			if (
+				AVAILABLE_LETTERS[
+					AVAILABLE_LETTERS.indexOf(tiles[selectedIndex]) + i
+				] === tiles[selectedIndex + i] ||
+				AVAILABLE_LETTERS[
+					AVAILABLE_LETTERS.indexOf(tiles[selectedIndex], 2) + i
+				] === tiles[selectedIndex + i]
+			) {
+				if (i === rightCharacters) {
+					rightCheck = true;
+				}
+				continue;
+			} else break;
+			rightCheck = true;
+		}
+		if (leftCharacters === 0) leftCheck = true;
+		for (let i = 1; i < leftCharacters + 1; i++) {
+			//check left side of AVAILABLE_LETTERS array
+			if (leftCharacters === 0) leftCheck = true;
+			if (
+				AVAILABLE_LETTERS[
+					AVAILABLE_LETTERS.indexOf(tiles[selectedIndex]) - i
+				] === tiles[selectedIndex - i] ||
+				AVAILABLE_LETTERS[
+					AVAILABLE_LETTERS.indexOf(tiles[selectedIndex], 2) - i
+				] === tiles[selectedIndex - i]
+			) {
+				if (i === leftCharacters || leftCharacters === 0) {
+					leftCheck = true;
+				}
+				continue;
+			} else break;
+		}
+		setIsWin(rightCheck && leftCheck ? true : false);
+		//check game status
+		if (isWin) {
+			console.log("You won!");
+		}
+	}, [selectedIndex, tiles, isWin]);
+
+	//This function will be invoked whenever a tile is clicked
 	const selectTile = (key: number) => {
+		// This function will read the keyboard and input character if it is part of "PURPLE"
 		const inputTile = (event: any) => {
 			let inputChar;
 			if (AVAILABLE_LETTERS.includes(event.key.toUpperCase())) {
 				inputChar = event.key.toUpperCase();
+				setSelectedIndex(key);
 			}
 			setTiles([
 				...tiles.slice(0, key),
@@ -22,9 +83,9 @@ const PlayScreen = () => {
 		};
 		if (tiles[key] === "") {
 			window.addEventListener("keydown", inputTile);
-			return () => window.removeEventListener("keydown", inputTile);
 		}
 	};
+
 	return (
 		<div className="grid grid-cols-12 gap-2 s:w-full md:w-1/2 lg:w-1/3 h-1/2">
 			{tiles.map((value, key) => (
