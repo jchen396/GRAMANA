29688fcf9b0bddcbd4d593743c7dd9e0f5e9ecfe
Jackie Chen <ecuojackie@yahoo.com>
2022-07-20 19:16:06 -0700

    can now access individual boxes in the grid

--- components/PlayScreen.tsx
+++ components/PlayScreen.tsx
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 
 
 
-const PlayScreen = () => {
-  let initialArr = []
+const PlayScreen= () => {
+  let initialArr:string[] = []
   for(let i = 0; i < 144; i++){
-    initialArr.push(null)
+    initialArr.push("")
   }
   const [tiles, setTiles] = useState(initialArr)
   console.log(tiles)
+  const selectTile = (key: number) => {
+    const clickText:string = 'P'
+    setTiles([
+    ...tiles.slice(0, key),
+    clickText,
+    ...tiles.slice(key + 1, tiles.length)
+  ])
+  }
   return <div className="grid grid-cols-12 gap-2 s:w-full md:w-1/2 lg:w-1/3 h-1/2">
     
     {tiles.map((value,key) => (
-      <div className="s:w-6 s:h-6 w-8 h-8 border-2 border-red-200 hover:border-blue-200" key={key}>{key+1}</div>
+      <div className="s:w-6 s:h-6 w-8 h-8 border-2 border-red-200 hover:border-blue-200 flex justify-center items-center" onClick={() => selectTile(key)} key={key}>{value}</div>
     )
     )}
 
