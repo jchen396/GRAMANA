dd92f94bfeecde1be472aeda9d931b81800a39ca
jchen396 <50890393+jchen396@users.noreply.github.com>
2022-11-16 23:34:03 -0800

    toggling game room now expands and shows join button

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "06ba988b6c3e9368"; }
+/******/ 		__webpack_require__.h = function() { return "195684d1cbbdf96d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/browse/index.tsx
+++ client/pages/browse/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import socket from "./../../components/Socket";
-import autoAnimate from "@formkit/auto-animate";
 
 const index = () => {
   const [gameRoomNames, setGameRoomNames] = useState<string[]>([]);
@@ -18,9 +17,6 @@ const index = () => {
       socket.off("showRooms");
     };
   }, []);
-  useEffect(() => {
-    parent.current[0]?.current && autoAnimate(parent.current[0].current);
-  }, [parent]);
   const joinRoomHandler = (name: string) => {
     router.push({
       pathname: "/",
@@ -43,17 +39,28 @@ const index = () => {
                 <div
                   ref={parent.current[key]}
                   key={key}
-                  className="my-4 py-4 px-2 w-4/5 bg-neutral-700 rounded-md flex flex-row justify-between items-center hover:bg-neutral-600 cursor-pointer"
+                  className={`my-4 py-4 px-4 w-4/5
+                   bg-neutral-700 rounded-md flex flex-col hover:bg-neutral-600 cursor-pointer transition ease-in-out delay-150
+                   ${showKey[key as keyof typeof showKey] && "h-1/4"}
+                   `}
                   onClick={() => reveal(key)}
                 >
-                  <h1 className="text-purple-400 text-lg ">
-                    Room: {name.toUpperCase()}
-                  </h1>
-                  <p className="text-emerald-400 text-sm ">
-                    Players: {gameRoomNames[name].length}
-                  </p>
+                  <div className="flex flex-row justify-between items-center ">
+                    <h1 className="text-purple-400 text-lg ">
+                      Room: {name.toUpperCase()}
+                    </h1>
+                    <p className="text-emerald-400 text-sm ">
+                      Players: {gameRoomNames[name].length}
+                    </p>
+                  </div>
+
                   {showKey[key as keyof typeof showKey] && (
-                    <p className="text-slate-100">test dropdown</p>
+                    <div>
+                      <p className="text-slate-100 ">test dropdown</p>
+                      <button className="p-2 text-slate-900 font-semibold text-lg bg-emerald-500 rounded-lg">
+                        Join
+                      </button>
+                    </div>
                   )}
                 </div>
               );
