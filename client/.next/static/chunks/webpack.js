07eb3c580c0413692c5f9eb7f76b9031adf58ed9
Jackie Chen <ecuojackie@yahoo.com>
2023-01-02 20:35:57 -0800

    Changing server to fix cors error

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18b04ebb9a95703c"; }
+/******/ 		__webpack_require__.h = function() { return "ea5129215012d017"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
