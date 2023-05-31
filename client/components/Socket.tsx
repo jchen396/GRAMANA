60fea7e1c9774e617f0af38a4f73a677fa17e1a8
Jackie Chen <ecuojackie@yahoo.com>
2023-05-31 12:11:42 -0700

    Fix dotenv imports

--- client/components/Socket.tsx
+++ client/components/Socket.tsx
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
+require("dotenv").config();
+console.log(process.env.NEXT_PUBLIC_SERVER_DOMAIN)
 const SERVER = process.env.NEXT_PUBLIC_SERVER_DOMAIN;
 const socket = io(SERVER);
 export default socket;
